ae32c1095db7db8dfa0c31f3e206df6404ceb862
zcappop <zcappop@CT00016389.ad.ucl.ac.uk>
2019-01-09 12:50:30 +0000

    Initial commit for exam2_17

---
+++ PHAS0056/src/exam2_17/ExamPart1.java
@@ -0,0 +1,10 @@
+package exam2_17;
+
+public class ExamPart1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ PHAS0056/src/exam2_17/Recording.java
@@ -0,0 +1,8 @@
+package exam2_17;
+
+public class Recording {
+	protected double samplingFq, sampleN, maxAmp;
+	
+	
+
+}
